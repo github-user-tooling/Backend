6d57b6515d41583319a172e4fb9c280960481ad8
William Sedlacek <wsedlacekc@gmail.com>
2019-11-15 23:49:18 -0600

    Set cookie settings
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/main.ts
+++ src/main.ts
@@ -36,6 +36,12 @@ async function bootstrap() {
       secret: environment.secret,
       resave: false,
       saveUninitialized: false,
+      cookie: {
+        secure: environment.production,
+        httpOnly: true,
+        maxAge: 1 * 24 * 60 * 60 * 1000,
+      },
+      rolling: true,
       store,
     })
   );
