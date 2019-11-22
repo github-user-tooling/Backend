56406bfbaaa40582f68a2a1b434994afda01d33d
William Sedlacek <wsedlacekc@gmail.com>
2019-11-22 09:44:14 -0600

    Create Examples of GitHub test
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- src/app/github/github.service.spec.ts
+++ src/app/github/github.service.spec.ts
@@ -1,5 +1,6 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
+import { environment } from '@env';
 import { NotesModule } from 'notes/notes.module';
 import { GithubService } from './github.service';
 
@@ -17,5 +18,22 @@ describe('GithubService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('login()', () => {
+    it('should provide ID', async () => {
+      const id = await service.login(environment.testingToken);
+      expect(id).toBeTruthy();
+      expect(typeof id).toBe('string');
+    });
+  });
+
+  describe('dashboard()', () => {
+    it('should provide user and following', async () => {
+      const id = 'MDQ6VXNlcjM2MjQwOTg=';
+      const dashboard = await service.dashboard(environment.testingToken, id, false);
+      expect(dashboard.user).toBeTruthy();
+      expect(dashboard.following).toBeTruthy();
+    });
   });
 });
